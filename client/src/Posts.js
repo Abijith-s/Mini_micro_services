f66532dc34ea3085f73f6e0ab656aac1eebc5e33
Abijith-s <abijithsebastian66@gmail.com>
2022-05-16 21:50:11 +0530

    card added for post

--- client/src/Posts.js
+++ client/src/Posts.js
@@ -1,19 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from 'react-bootstrap/Card';
 
 const Posts = () => {
-const [post,setPots] = useState({});
-const fetchposts =async ()=>{
-    const res = await axios.get('http://localhost:4000/posts');
-    setPots(res.data)
-    console.log("data",res.data);
-}
-useEffect(()=>{
+  const [post, setPots] = useState({});
+  const fetchposts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+    setPots(res.data);
+  };
+  useEffect(() => {
     fetchposts();
-},[]);
+  }, []);
   return (
-    <div/>
+    <div className="container mt-3">
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Card Subtitle
+          </Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
   );
-}
+};
 
 export default Posts;
