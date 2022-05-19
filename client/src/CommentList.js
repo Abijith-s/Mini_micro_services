27dc7261d6861faa5d30049aec8fbd64e889b6e9
Abijith-s <abijithsebastian66@gmail.com>
2022-05-20 01:01:24 +0530

    moderation rendered in client side

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -4,7 +4,21 @@ const CommentList = ({comments}) => {
   return (
     <div>
       {
-        comments?.map(e =>  <p>{e.content}</p>)
+        comments?.map((e) =>  {
+         
+        let content;
+        if(e.status === 'approved'){
+          content = e.content;
+        }
+        if(e.status === 'pending'){
+          content = 'The comment is awaiting moderation';
+        }
+        if(e.status === 'rejected'){
+          content = 'The comment has been rejected';
+        }
+        return <p>{content}</p>
+      
+      })
       }
     </div>
   )
