992d031e00cd538f194bd868c5cf666f496bcd3e
Abijith-s <abijithsebastian66@gmail.com>
2022-05-20 01:40:51 +0530

    event sync added

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
  app.use(bodyParser.json());
@@ -42,7 +43,7 @@ const app = express();
 
 app.listen(4002,async()=>{
     console.log("listening to port 4002");
-    await axios.get('http://localhost:4005/events');
+   const res = await axios.get('http://localhost:4005/events');
     for(let event of res.data){
         console.log("proccessing event",event.type);
         handleEvents(event.type,event.data);
