f06efa87a881109cc2b65d7d33a0b3c48cb42e30
Abijith-s <abijithsebastian66@gmail.com>
2022-05-20 00:24:40 +0530

    status bug fixed

--- comments/index.js
+++ comments/index.js
@@ -41,14 +41,14 @@ app.post("/events", async (req, res) => {
   const { type, data } = req.body;
 
   if (type === "CommentModerated") {
-    const { postId, id, status } = data;
-    comments = commentsByPostId[postId];
+    const { postId, id, status,content } = data;
+    const comments = commentsByPostId[postId];
 
     const comment = comments.find((comment) => {
       return comment.id === id;
     });
     comment.status = status;
-    await axios.post("http://localhost:4005", {
+    await axios.post("http://localhost:4005/events", {
       type: "CommentUpdated",
       data: {
         id,
